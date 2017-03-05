0f89809bf405e785c6ad1392485699aadd788592
Eric Tan <ejtan@carbon.localdomain>
2017-03-04 16:12:26 -0800

    decoder working, off by 1 bug tho due to floating point

--- ProbA.R
+++ ProbA.R
@@ -1,26 +1,26 @@
 library(pixmap)
 
 # Encoder
-secretencoder <- function(imagefilenmame, msg, startpix, stride, consec=NULL)
+secretencoder <- function(imagefilename, msg, startpix, stride, consec=NULL)
 {
     # Check if file exists
-    if (!file.exists(imagefilenmame)) {
+    if (!file.exists(imagefilename)) {
         print("File does not exist.")
         stop()
     }
     
     # Read image
-    img <- read.pnm(imagefilenmame)
-    plot(img)
+    img <- read.pnm(imagefilename)
     grey_img <- img@grey
     
     # Split string and convert to doubles
     # Vectorized since utf8ToInt() returns a vector if string is inputed
     str_val <- utf8ToInt(msg) / 128
+    str_val <- c(str_val, 0)
     
     # Encode
     j <- 1
-    for (i in seq(startpix, startpix * length(str_val), stride)) {
+    for (i in seq(startpix, startpix + (stride * length(str_val)), stride)) {
         grey_img[i] <- str_val[j]
         j <- j + 1
     }
@@ -29,8 +29,6 @@ secretencoder <- function(imagefilenmame, msg, startpix, stride, consec=NULL)
     img@grey <- grey_img
     write.pnm(img)
     
-    
-    
     return(img)
 }
 
@@ -38,4 +36,21 @@ secretencoder <- function(imagefilenmame, msg, startpix, stride, consec=NULL)
 # Decoder
 secretdecoder <- function(imagefilename, startpix, stride, consec=NULL)
 {
+    img <- read.pnm(imagefilename)
+    msg_mat <- img@grey
+    size <- length(msg_mat)
+    
+    idx_arr <- seq(startpix, size, stride)
+    msg_vals <- msg_mat[idx_arr]
+    nul_idx <- which(msg_vals == 0)
+    
+    # nul_idx can have more than one index, so since everything
+    # in R is a vector, I can access the first instance of 0 reguardless
+    # of how many 0's I pick up
+    print(nul_idx)
+    msg_vals <- msg_vals[1:(nul_idx[1] - 1)]
+    msg_vals <- msg_vals * 128
+    
+    msg <- intToUtf8(as.integer(msg_vals), multiple=TRUE)
+    print(msg)
 }

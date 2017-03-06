2a20cbe240a9591153d0d7e0b9b4397f8b6171e1
Eric Tan <ejtan@carbon.localdomain>
2017-03-05 21:48:24 -0800

    Added warnings where approiate

--- ProbA.R
+++ ProbA.R
@@ -1,12 +1,23 @@
 # Required libries
 library(pixmap)
 
+# Helper function for computing GCD using euclid's algorithm with recursion
+# Algorithm pseudocode from CLRS, Introduction to Algorithms 3rd. Edition, pg. 935
+euclid <- function(a, b)
+{
+    if (b == 0)
+        return(a)
+    else
+        return(euclid(b, a %% b))
+}
+
+
 # Encoder
 secretencoder <- function(imagefilename, msg, startpix, stride, consec=NULL)
 {
     # Check if file exists
     if (!file.exists(imagefilename)) {
-        print("File does not exist.")
+        warning("File does not exist!")
         stop()
     }
     
@@ -14,23 +25,31 @@ secretencoder <- function(imagefilename, msg, startpix, stride, consec=NULL)
     img <- read.pnm(imagefilename)
     grey_img <- img@grey
     
+    
+    # Check if stride is relatively prime to image size
+    # Relatively prime means 2 ints have gcd(a, b) = 1.
+    size <- ncol(grey_img) * nrow(grey_img)
+    if (euclid(stride, size) != 1) {
+        warning("Stride not relatively prime to image size!")
+        stop()
+    }
+    
     # Split string and convert to doubles
     # Vectorized since utf8ToInt() returns a vector if string is inputed
     str_val <- as.double(utf8ToInt(msg)) / 128.0
-    str_val <- c(str_val, 0)
-    print(str_val)
+    str_val <- c(str_val, 0) # Add null terminator
     
     # Encode
-    j <- 1
-    for (i in seq(startpix, startpix + (stride * length(str_val)), stride)) {
-        grey_img[i] <- str_val[j]
-        j <- j + 1
-    }
+    # Generate a sequencce of indices to store into grey_img. Note that the
+    # lengh(str_val) - 1 is needed becaise seq generates [start, end], so I have 1 extra
+    encode_idx <- seq(startpix, startpix + (stride * (length(str_val) - 1)), stride)
+    grey_img[encode_idx] <- str_val
     
     # Overwrite grey in image and save to disc
     img@grey <- grey_img
     write.pnm(img)
     
+    # Returns a pixmapGrey class
     return(img)
 }
 
@@ -38,6 +57,13 @@ secretencoder <- function(imagefilename, msg, startpix, stride, consec=NULL)
 # Decoder
 secretdecoder <- function(imagefilename, startpix, stride, consec=NULL)
 {
+    # Check if file exists
+    if (!file.exists(imagefilename)) {
+        warning("File does not exist!")
+        stop()
+    }
+    
+    # Read image
     img <- read.pnm(imagefilename)
     msg_mat <- img@grey
     
@@ -53,16 +79,17 @@ secretdecoder <- function(imagefilename, startpix, stride, consec=NULL)
         msg_vals <- c(msg_vals, val)
     }
     
-    
-    # Revert from floats back to ints for Utf8 conversion
+    # Revert from floats back to ints for Utf8 conversion. Rounding is necessary
     # While loop which picks up message takes the null terminator,
     # so I remove it by doing msg_vals[,-1]
-    print(msg_vals[-length(msg_vals)] * 128)
     msg_vals <- round(msg_vals[-length(msg_vals)] * 128)
     
-    # FLOATING POINT BUG? FIX LATER
-    msg <- intToUtf8(msg_vals, multiple=TRUE)
-    print(msg)
+    # Decode message.
+    if (length(msg_vals) == 1) # Special case, encode 1 chracter
+        msg <- intToUtf8(msg_vals)
+    else
+        msg <- intToUtf8(msg_vals, multiple=TRUE)
     
+    # Returns a vector of chars
     return(msg)
 }
